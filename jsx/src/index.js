eb6088935d1a1e5dbebc12f7d03c30f3627c6dcf
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-04-05 15:46:46 -0400

    HTML comments

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -11,20 +11,44 @@ const App = () => {
  */
 
   const buttonExampleObj = { text: "New Value" };
+  function avatarId() {
+    let text;
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < 10; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <div>
-        <label className="label" htmlFor="name">
-          Enter name
-        </label>
-        <input id="name" type="text" />
-        <button style={{ color: "red" }}>{buttonExampleObj.text}</button>
-        {/* <button style={{ color: "red" }}>{getButtonValue()}</button> */}
-        {/*      <button style={{ color: "red" }}>{buttonExample}</button> */}
+    <>
+      <div className="ui container comments">
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img
+              src={`https://avatars.dicebear.com/4.5/api/male/${avatarId()}.svg`}
+              alt="avatar"
+            />
+          </a>
+
+          <div className="content">
+            <a href="/" className="author">
+              Paul
+            </a>
+            <div className="metadata">
+              <span className="date">Today at 6PM</span>
+            </div>
+            <div className="text">My first post</div>
+          </div>
+        </div>
       </div>
-    </div>
+
+      {/*       <button style={{ color: "red" }}>{buttonExampleObj.text}</button> */}
+      {/* <button style={{ color: "red" }}>{getButtonValue()}</button> */}
+      {/*      <button style={{ color: "red" }}>{buttonExample}</button> */}
+    </>
   );
 };
 
