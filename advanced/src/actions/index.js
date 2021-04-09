adabcd0d8949e9c7a1893d032de2fe18c4651e12
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-04-09 18:43:52 -0400

    Redux Thunk & lodash

--- advanced/src/actions/index.js
+++ advanced/src/actions/index.js
@@ -1,6 +1,24 @@
 import fetchAPI from "../api/fetchAPI";
 import _ from "lodash";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // dispatch to redux thunk will invoce and pass it as first argument
+  console.log("Pre - fetched");
+
+  await dispatch(fetchPosts());
+  //console.log(getState().posts); // All posts fetched
+  /*   const userIds = _.uniq(_.map(getState().posts, "userId")); // fetch and map unique id */
+  //console.log(userIds);
+  //userIds.forEach((id) => dispatch(fetchUser(id)));
+  console.log("Post - fetched");
+  // if chain then map or other then argument will be getState().posts automatically
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value(); // lodash mandatory
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await fetchAPI.get("/posts");
 
