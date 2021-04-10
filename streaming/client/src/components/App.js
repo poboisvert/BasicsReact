6de3197f013eb49de430951038d6a86018d4875f
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-04-09 23:50:11 -0400

    Auth2 sign In & Out

--- streaming/client/src/components/App.js
+++ streaming/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import Create from './streams/Create';
 import Delete from './streams/Delete';
@@ -10,23 +10,25 @@ import Show from './streams/Show';
 const App = () => {
   return (
     <>
-      <Header />
-      <header class='bg-white shadow'>
-        <div class='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8'>
-          <h1 class='text-3xl font-bold text-gray-900'>Dashboard</h1>
+      <header className='bg-white shadow'>
+        <div className='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8'>
+          <h1 className='text-3xl font-bold text-gray-900'>Dashboard</h1>
         </div>
       </header>
       <main>
-        <div class='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+        <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
           <BrowserRouter>
-            <Route path='/' exact component={List} />
-            <Route path='/streams/edit' exact component={Edit} />
-            <Route path='/streams/delete' exact component={Delete} />
-            <Route path='/streams/show' exact component={Show} />
-            <Route path='/streams/create' exact component={Create} />
+            <div>
+              <Header />
+              <Route path='/' exact component={List} />
+              <Route path='/streams/new' exact component={Create} />
+              <Route path='/streams/edit' exact component={Edit} />
+              <Route path='/streams/delete' exact component={Delete} />
+              <Route path='/streams/show' exact component={Show} />
+            </div>
           </BrowserRouter>
-          <div class='px-4 py-6 sm:px-0'>
-            <div class='border-4 border-dashed border-gray-200 rounded-lg h-96'></div>
+          <div className='px-4 py-6 sm:px-0'>
+            <div className='border-4 border-dashed border-gray-200 rounded-lg h-96'></div>
           </div>
         </div>
       </main>
---
+++ streaming/client/src/components/GoogleAuth.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+export default class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+  // Already render then call componentDidMount
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
+          clientId:
+            '589082165700-mupj2ueq98bjgkcg27er8qshs6ao7bnd.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Can not confirm if singed in</div>;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button
+          type='button'
+          onClick={this.onSignOut}
+          className='focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-red-500 hover:bg-red-600 hover:shadow-lg flex items-center'
+        >
+          <svg
+            className='w-4 h-4 mr-2'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01'
+            />
+          </svg>
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button
+          type='button'
+          onClick={this.onSignIn}
+          className='focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-green-500 hover:bg-green-600 hover:shadow-lg flex items-center'
+        >
+          <svg
+            className='w-4 h-4 mr-2'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5'
+            />
+          </svg>
+          Sign In
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
